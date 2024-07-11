984e32a4468d4c0f0288874362bd9dffd2ec0496
Arthur Lluisma <ajclluisma@gmail.com>
2024-07-11 20:35:01 +0800

    style: change nav and header styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,29 +8,37 @@ import { useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Main() {
-    const [searchValue, setSearchValue] = useState("");
-    return (
-        <>
-            <div className={styles.mainHeader}>
-                <div className={styles.headerBG}>
-                    <div className={styles.headerBGOverlay}></div>
-                </div>
-                <div className={styles.headerSearch}>
-                    <p className={styles.searchMessage}>Search for the art you want, from the creators you love.</p>
-                    <button className={styles.searchButton}>
-                        <div className={styles.searchButtonInner}>
-                            <FontAwesomeIcon style={{ fontSize: "30px" }} icon={faSearch}></FontAwesomeIcon>
-                        </div>
-                    </button>
-                    <input
-                        className={styles.searchBar}
-                        type="text"
-                        value={searchValue}
-                        onChange={(e) => setSearchValue(e.target.value)}
-                    ></input>
-                </div>
+  const [searchValue, setSearchValue] = useState("");
+  return (
+    <>
+      <div className={styles.mainHeader}>
+        <div className={styles.headerBG}>
+          <div className={styles.headerBGOverlay}></div>
+        </div>
+        <div className={styles.headerSearch}>
+          <p className={styles.searchMessage}>
+            An organization that focuses on promoting sustainable initiatives.
+          </p>
+          <button className={styles.searchButton}>
+            <div className={styles.searchButtonInner}>
+              <FontAwesomeIcon
+                style={{ fontSize: "30px" }}
+                icon={faSearch}
+              ></FontAwesomeIcon>
             </div>
-            <Feed search={searchValue.toLowerCase()} isShowEverything={searchValue === ""} />
-        </>
-    );
+          </button>
+          <input
+            className={styles.searchBar}
+            type="text"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          ></input>
+        </div>
+      </div>
+      <Feed
+        search={searchValue.toLowerCase()}
+        isShowEverything={searchValue === ""}
+      />
+    </>
+  );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,32 +1,27 @@
-import Image from "next/image";
 import Link from "next/link";
 import styles from "co-mission/styles/Nav.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component
-import { faAngleDown } from "@fortawesome/free-solid-svg-icons"; // import the icons you need
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
-    return (
-        <div className={styles.mainNav}>
-            <Link href="/" className={styles.navLeft}>
-                <Image className={styles.logo} src="/comission-logo2.png" fill alt="comission" />
-            </Link>
-
-            {/* div for other buttons at right of nav */}
-            <div className={styles.navRight}>
-                <div className={styles.exploreButton}>
-                    {/* how do i put a dropdown menu here */}
-                    {/* TODO: get icon from fontawesome */}
-                    <p className={styles.explore}>Explore</p>
-                    <FontAwesomeIcon
-                        className={styles.exploreAngle}
-                        style={{ fontSize: "20px" }}
-                        icon={faAngleDown}
-                    ></FontAwesomeIcon>
-                </div>
-                <div className={styles.joinButton}>
-                    <p className={styles.join}>Join</p>
-                </div>
-            </div>
+  return (
+    <div className={styles.mainNav}>
+      <Link href="/" className={styles.navLeft}>
+        <p className={styles.logo}>EcoCharge QC</p>
+      </Link>
+      <div className={styles.navRight}>
+        <div className={styles.exploreButton}>
+          <p className={styles.explore}>Explore</p>
+          <FontAwesomeIcon
+            className={styles.exploreAngle}
+            style={{ fontSize: "20px" }}
+            icon={faAngleDown}
+          ></FontAwesomeIcon>
         </div>
-    );
+        <div className={styles.joinButton}>
+          <p className={styles.join}>Join</p>
+        </div>
+      </div>
+    </div>
+  );
 }
